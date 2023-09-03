13063149f0241853f0826a07b7ecc52fee88c329
iara-barreiro <141201163+iara-barreiro@users.noreply.github.com>
2023-09-02 22:09:10 -0300

    commit2

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  const soloStrings = strangeArray.filter((element) => typeof element === "string");
+  soloStrings.sort((a, b) => a.toUpperCase().localeCompare(b.toUpperCase()));
+  showList(soloStrings);
+});
+
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
